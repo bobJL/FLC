a4fa503c4adfd83b3e5b86d32ef789f2e3997593
bobJL <bob.-lubbers@hotmail.com>
2019-06-11 20:43:34 +0200

    event pagina 0.3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,11 +95,11 @@
         </style>
     </head>
     <body>
-    <header id="header" class="header">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
         <div class="same"> 
         <h3 class=""> FCL evenement planner</h3>
-    <nav class="col-12 links nav" style="--items: 5;
-    background-color:#f0f0f5;">
+ 
 		<a href="#">Link 1</a>
 		<a href="#">Link 2</a>
 		<a href="#">Link 3</a>
@@ -111,12 +111,89 @@
 </header>
  
 
-            <div class="content">
-               
-                <div class="evenementen">
-                    <div class="event">
-                    <h2 class="eventname">voorbeeld</h2>
-                    </div>
+        
+                 <!-- Page Content -->
+  <div class="container" style="padding-top: 25px;">
+
+<div class="row">
+
+  <!-- Blog Entries Column -->
+  <div class="col-md-8">
+
+    <h1 class="my-4">Toekomstige evenementen
+      <small>alle nodige informatie</small>
+    </h1>
+
+    <!-- Blog Post -->
+    <div class="card mb-4">
+      <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">evenement naam</h2>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+        <a href="#" class="btn btn-primary">lees meer &rarr;</a>
+      </div>
+      <div class="card-footer text-muted">
+            vind plaats op Januari 2020 
+        <a href="#">zie meer over dit</a>
+      </div>
+    </div>
+
+    <!-- Blog Post -->
+    <div class="card mb-4">
+      <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">evenement naam</h2>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+        <a href="#" class="btn btn-primary">lees meer &rarr;</a>
+      </div>
+      <div class="card-footer text-muted">
+            vind plaats op Januari 2020 
+        <a href="#">zie meer over dit</a>
+      </div>
+    </div>
+
+    <!-- Blog Post -->
+    <div class="card mb-4">
+      <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+      <div class="card-body">
+        <h2 class="card-title">evenement naam</h2>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+        <a href="#" class="btn btn-primary">lees meer &rarr;</a>
+      </div>
+      <div class="card-footer text-muted">
+            vind plaats op Januari 2020 
+        <a href="#">zie meer over dit</a>
+      </div>
+    </div>
+
+    <!-- Pagination -->
+    <ul class="pagination justify-content-center mb-4">
+      <li class="page-item">
+        <a class="page-link" href="#">&larr; Older</a>
+      </li>
+      <li class="page-item disabled">
+        <a class="page-link" href="#">Newer &rarr;</a>
+      </li>
+    </ul>
+
+  </div>
+
+  <!-- Sidebar Widgets Column -->
+  <div class="col-md-4" style="padding-top:68px;">
+
+    <!-- Search Widget -->
+    <div class="card my-4">
+      <h5 class="card-header">Search</h5>
+      <div class="card-body">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search for...">
+          <span class="input-group-btn">
+            <button class="btn btn-secondary" type="button">Go!</button>
+          </span>
+        </div>
+      </div>
+    </div>
+
                 </div>
 
                
